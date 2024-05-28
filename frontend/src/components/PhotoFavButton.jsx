6674573342e99c264cde614dee1ad3a72a57da86
Monica Palko <m.a.palko07@gmail.com>
2024-05-27 22:39:56 -0400

    user can like a photo

--- frontend/src/components/PhotoFavButton.jsx
+++ frontend/src/components/PhotoFavButton.jsx
@@ -1,13 +1,17 @@
-import React, { useCallback, useState } from 'react';
-
-import FavIcon from './FavIcon';
+import React, { useState } from 'react';
+import FavIcon from './FavIcon'; 
 import '../styles/PhotoFavButton.scss';
 
-function PhotoFavButton() {
+function PhotoFavButton({ isFavorite, toggleFavorite }) {
+  // Function to handle click event
+  const handleClick = () => {
+    toggleFavorite();
+  };
+
   return (
-    <div className="photo-list__fav-icon">
+    <div className={`photo-list__fav-icon ${isFavorite ? 'active' : ''}`} onClick={handleClick}>
       <div className="photo-list__fav-icon-svg">
-        {/* Insert React */}
+        <FavIcon />
       </div>
     </div>
   );
--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -1,16 +1,27 @@
-import React from "react";
-import "../styles/PhotoListItem.scss";
+import React, { useState } from 'react';
+import PhotoFavButton from './PhotoFavButton';
+import '../styles/PhotoListItem.scss';
 
 const PhotoListItem = ({ username, imageSource, id, location, profile }) => {
+  const [isFavorite, setIsFavorite] = useState(false);
+
+  // Function to toggle favorite status
+  const toggleFavorite = () => {
+    setIsFavorite(!isFavorite);
+  };
+
   return (
-    <div className="photo-list__item">
+    <div className={`photo-list__item ${isFavorite ? 'favorite' : ''}`}>
       <img src={imageSource} alt={`Photo by ${username}`} />
       <div className="photo-details">
         <img src={profile} alt={`Profile picture of ${username}`} className="profile-picture" />
         <div className="username">{username}</div>
         <div className="location">{location.city}, {location.country}</div>
         <div className="id">ID: {id}</div>
-
+        <PhotoFavButton
+          isFavorite={isFavorite}
+          toggleFavorite={toggleFavorite}
+        />
       </div>
     </div>
   );
