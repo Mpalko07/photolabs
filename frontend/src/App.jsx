da16b9565b4f1ce3e466689de80c4c9bf42d14e6
Monica Palko <m.a.palko07@gmail.com>
2024-05-29 17:28:52 -0400

    added mock data

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import HomeRoute from './routes/HomeRoute';
+import photos from './mocks/photos';
+import topics from './mocks/topics';
 import './App.scss';
 
-const App = () => (
-  <div className="App">
+const App = () => {
+  return (
+    <div className="App">
     {/* { Array.from(Array(3)).map((_, index) => <PhotoListItem key={index}/>) } */}
-    <HomeRoute/>
-  </div>
-);
+    <HomeRoute photos={photos} topics={topics} />
+    </div>
+  );
+};
 
 export default App;
