d83140cbcd5f357076232deb52473cfb77a00ba0
Monica Palko <m.a.palko07@gmail.com>
2024-05-29 16:21:42 -0400

    amended App.jsx to remove unnecessary imports

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,4 @@
 import React from 'react';
-import PhotoList from './components/PhotoList';
-import TopNavigationBar from './components/TopNavigationBar';
 import HomeRoute from './routes/HomeRoute';
 import './App.scss';
 
@@ -8,8 +6,6 @@ const App = () => (
   <div className="App">
     {/* { Array.from(Array(3)).map((_, index) => <PhotoListItem key={index}/>) } */}
     <HomeRoute/>
-    <TopNavigationBar/>
-    <PhotoList/>
   </div>
 );
 
