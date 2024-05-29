49c504c71e4fad80a387a021fbb2566af7e1f270
Monica Palko <m.a.palko07@gmail.com>
2024-05-29 16:19:11 -0400

    created HomeRoute component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import PhotoList from './components/PhotoList';
 import TopNavigationBar from './components/TopNavigationBar';
+import HomeRoute from './routes/HomeRoute';
 import './App.scss';
 
 const App = () => (
   <div className="App">
     {/* { Array.from(Array(3)).map((_, index) => <PhotoListItem key={index}/>) } */}
+    <HomeRoute/>
     <TopNavigationBar/>
     <PhotoList/>
   </div>
--- frontend/src/routes/HomeRoute.jsx
+++ frontend/src/routes/HomeRoute.jsx
@@ -1,11 +1,14 @@
+// frontend/src/components/HomeRoute.jsx
 import React from 'react';
-
-import '../styles/HomeRoute.scss';
+import TopNavigationBar from '../components/TopNavigationBar'; // Import TopNavigationBar component
+import PhotoList from '../components/PhotoList'; // Import PhotoList component
+import '../styles/HomeRoute.scss'; // Import styles for HomeRoute
 
 const HomeRoute = () => {
   return (
     <div className="home-route">
-      {/* Insert React */}
+      <TopNavigationBar /> 
+      <PhotoList /> 
     </div>
   );
 };
