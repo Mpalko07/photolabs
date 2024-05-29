aa5e4e3228e10c53790d32cf5b42a8e90d6c0ef6
Monica Palko <m.a.palko07@gmail.com>
2024-05-29 18:06:24 -0400

    add logic to store liked photos

--- frontend/src/routes/HomeRoute.jsx
+++ frontend/src/routes/HomeRoute.jsx
@@ -1,15 +1,34 @@
-// frontend/src/components/HomeRoute.jsx
-import React from 'react';
+import React, { createContext, useContext, useState } from 'react';
 import TopNavigationBar from '../components/TopNavigationBar';
 import PhotoList from '../components/PhotoList';
 import '../styles/HomeRoute.scss';
 
+// Create a context for managing favourites
+const FavouritesContext = createContext();
+
+// Custom hook to access the FavouritesContext
+export const useFavourites = () => useContext(FavouritesContext);
 
 const HomeRoute = ({ photos, topics }) => {
+  // State to store favourited photo IDs
+  const [favourites, setFavourites] = useState([]);
+
+  // Function to add a photo to favourites
+  const addFavourite = (photoId) => {
+    setFavourites([...favourites, photoId]);
+  };
+
+  // Function to remove a photo from favourites
+  const removeFavourite = (photoId) => {
+    setFavourites(favourites.filter((id) => id !== photoId));
+  };
+
   return (
     <div className="home-route">
-      <TopNavigationBar topics={topics} />
-      <PhotoList photos={photos} />
+      <FavouritesContext.Provider value={{ favourites, addFavourite, removeFavourite }}>
+        <TopNavigationBar topics={topics} />
+        <PhotoList photos={photos} />
+      </FavouritesContext.Provider>
     </div>
   );
 };
