16ac3efe71cd4c71cce936cc2cad8d67e0b8c526
Monica Palko <m.a.palko07@gmail.com>
2024-05-29 18:28:45 -0400

    update the TopNavigationBar to display a notification

--- frontend/src/components/TopNavigationBar.jsx
+++ frontend/src/components/TopNavigationBar.jsx
@@ -1,12 +1,17 @@
 import React from 'react';
+import { useFavourites } from '../routes/HomeRoute';
 import TopicList from './TopicList';
-import FavBadge from './FavBadge';
 import '../styles/TopNavigationBar.scss';
 
 const TopNavigationBar = ({ topics }) => {
+  const { favourites } = useFavourites();
+
   return (
     <div className="top-nav-bar">
       <span className="top-nav-bar__logo">PhotoLabs</span>
+      {favourites.length > 0 && (
+        <div className="top-nav-bar__notification">{favourites.length}</div>
+      )}
       <TopicList topics={topics} />
     </div>
   );
