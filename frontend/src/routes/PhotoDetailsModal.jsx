9f09feecba2efc9de32c653f117599ac739b204f
Monica Palko <m.a.palko07@gmail.com>
2024-05-29 22:10:57 -0400

    blank modal will close when a user clicks the 'x' button

--- frontend/src/routes/PhotoDetailsModal.jsx
+++ frontend/src/routes/PhotoDetailsModal.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import closeSymbol from '../assets/closeSymbol.svg';
+import '../styles/PhotoDetailsModal.scss';
 
 const PhotoDetailsModal = ({ onClose }) => {
   return (
     <div className="modal">
       <div className="modal-content">
-        <button className="modal-close-button" onClick={onClose}>
+        <button className="photo-details-modal__close-button" onClick={onClose}>
           <img src={closeSymbol} alt="close symbol" />
         </button>
       </div>
