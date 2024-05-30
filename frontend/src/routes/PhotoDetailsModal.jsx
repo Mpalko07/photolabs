f6ab2e1c6bb621748edfe9757b6c474e889862b1
Monica Palko <m.a.palko07@gmail.com>
2024-05-29 20:32:32 -0400

    create blank modal

--- frontend/src/routes/PhotoDetailsModal.jsx
+++ frontend/src/routes/PhotoDetailsModal.jsx
@@ -1,16 +1,16 @@
 import React from 'react';
-
-import '../styles/PhotoDetailsModal.scss'
 import closeSymbol from '../assets/closeSymbol.svg';
 
-const PhotoDetailsModal = () => {
+const PhotoDetailsModal = ({ onClose }) => {
   return (
-    <div className="photo-details-modal">
-      <button className="photo-details-modal__close-button">
-        <img src={closeSymbol} alt="close symbol" />
-      </button>
+    <div className="modal">
+      <div className="modal-content">
+        <button className="modal-close-button" onClick={onClose}>
+          <img src={closeSymbol} alt="close symbol" />
+        </button>
+      </div>
     </div>
-  )
+  );
 };
 
 export default PhotoDetailsModal;
